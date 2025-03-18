2027838af87e41cbc242a67982af46aaa3f0e51b
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-03-18 16:00:44 -0230

    Added TournamentRepository for managing tournament data with all the required queries.

---
+++ src/main/java/com/keyin/rest/tournament/TournamentRepository.java
@@ -0,0 +1,24 @@
+package com.keyin.rest.tournament;
+
+import com.keyin.rest.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
+
+    Optional<Tournament> findTournamentByStartDate (LocalDate startDate);
+
+    Optional<Tournament> findTournamentByEndDate (LocalDate endDate);
+
+    List<Tournament> findTournamentByLocation(String location);
+
+    List<Tournament> findTournamentByCashPrizeAmount(double cashPrizeAmount);
+
+    List<Tournament> findTournamentByParticipatingMembers(List<Member> participatingMembers);
+
+}
