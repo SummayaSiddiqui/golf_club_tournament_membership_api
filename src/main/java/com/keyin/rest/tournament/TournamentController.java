0473769e8d9915ef776592cd2e348129594fc4ee
SummayaSiddiqui <summaya.siddiqui@keyin.com>
2025-04-02 13:39:12 -0230

    Added cross origin to allow react to fetch data

--- src/main/java/com/keyin/rest/tournament/TournamentController.java
+++ src/main/java/com/keyin/rest/tournament/TournamentController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/tournaments")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TournamentController {
 
     private final TournamentService tournamentService;
